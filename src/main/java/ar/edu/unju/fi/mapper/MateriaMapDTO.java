2f158da1c0e3cf890a25c0739c75304637acc6ba
vilte <nicolasvilte08@gmail>
2024-06-25 20:16:06 -0300

    Inicialidado Proyecto Grupal TPN5 Grupo 213. Vilte Nicolas

---
+++ src/main/java/ar/edu/unju/fi/mapper/MateriaMapDTO.java
@@ -0,0 +1,34 @@
+package ar.edu.unju.fi.mapper;
+
+import java.util.List;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+import ar.edu.unju.fi.dto.MateriaDTO;
+import ar.edu.unju.fi.model.Materia;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface MateriaMapDTO {
+
+	@Mapping(source = "codigo", target = "codigo")
+	@Mapping(source = "nombre", target = "nombre")
+	@Mapping(source = "curso", target = "curso")
+	@Mapping(source = "cantidadHoras", target = "cantidadHoras")
+	@Mapping(source = "modalidad", target = "modalidad")
+	@Mapping(source = "docente", target = "docente")
+	@Mapping(source = "carrera", target = "carrera")
+	@Mapping(source = "estado", target = "estado")
+
+	MateriaDTO convertirMateriaAMateriaDTO(Materia m);
+
+	@InheritInverseConfiguration
+	Materia convertirMateriaDTOAMateria(MateriaDTO m);
+
+	List<MateriaDTO> convertirListaMateriaAListaMateriaDTO(List<Materia> listaM);
+
+	List<Materia> convertirListaMateriaDTOAListaMateria(List<MateriaDTO> listaMDTO);
+
+}
