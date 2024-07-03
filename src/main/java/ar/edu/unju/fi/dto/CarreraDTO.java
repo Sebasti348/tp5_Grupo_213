197a3853707eed05f7b525d6f7989ba19b2089c8
usuario <usuario@DESKTOP-DM94O17>
2024-07-03 12:00:47 -0300

    Clase CarreraDTO

---
+++ src/main/java/ar/edu/unju/fi/dto/CarreraDTO.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.dto;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Component
+public class CarreraDTO {
+	private String codigo;
+	private String nombre;
+	private int cantidadHoras;
+	private Boolean estado;
+}
